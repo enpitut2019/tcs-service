5311ca54d7a3fc309401f307e7f52e0014b87787
MokkeMeguru <meguru.mokke@gmail.com>
2019-11-15 17:39:14 +0900

    insert bgug

--- src/task_cabinet_server/service/users.clj
+++ src/task_cabinet_server/service/users.clj
@@ -82,12 +82,13 @@
         id (-> path-params :id Integer/parseInt)]
     (if-not (and (s/valid? ::tokens/token authorization) (s/valid? ::users/id id ))
       {:status 400}
-      (if-not (token/check-token-exists? db id authorization)
-        {:status 403}
-        {:status 200
-         :body
-         {:result
-          (select-keys (usql/get-user db :id id) [:name :created_at :email])}}))))
+      (do (println (token/check-token-exists? db id authorization))
+       (if-not (token/check-token-exists? db id authorization)
+         {:status 403}
+         {:status 200
+          :body
+          {:result
+           (select-keys (usql/get-user db :id id) [:name :created_at :email])}})))))
 
 (s/def ::update-user-info (s/keys :req-un [::users/name ::users/email ::users/password]))
 (defn update-user-info-handler
