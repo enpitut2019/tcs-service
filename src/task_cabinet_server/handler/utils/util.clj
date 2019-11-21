02d9716c5085965694c79ec1ff0defc80a6c53aa
Mokke Meguru <meguru.mokke@gmail.com>
2019-11-21 16:05:40 +0900

    Update util.clj

--- src/task_cabinet_server/handler/utils/util.clj
+++ src/task_cabinet_server/handler/utils/util.clj
@@ -6,7 +6,7 @@
 ;; https://localhost
 
 (def default-origin
-  [#"http://localhost" #"https://localhost" #"https://enpitut2019.github.io"])
+  [#"http://localhost:4200" #"https://localhost:4200" #"http://localhost" #"https://localhost" #"https://enpitut2019.github.io"])
 
 (defn cors-handler [handler]
   (wrap-cors handler :access-control-allow-origin default-origin))
