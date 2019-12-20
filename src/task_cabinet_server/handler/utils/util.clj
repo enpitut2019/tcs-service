c5544649bb83eed885cb749e779f387f8f29274d
MokkeMeguru <meguru.mokke@gmail.com>
2019-12-20 06:52:14 +0000

    add-creadential

--- src/task_cabinet_server/handler/utils/util.clj
+++ src/task_cabinet_server/handler/utils/util.clj
@@ -20,7 +20,8 @@
     (timbre/warn "Access Origin: " (-> request :headers (get "origin")))
     (let [response (handler request)]
       (-> response
-          (assoc-in [:headers "Access-Control-Allow-Origin"] "https://enpitut2019.github.io")
+          (assoc-in [:headers "Access-Control-Allow-Credentials"] "true")
+          (assoc-in [:headers "Access-Control-Allow-Origin"] "*")
           (assoc-in [:headers "Access-Control-Allow-Headers"] "Content-Type")
           (assoc-in [:headers "Access-Control-Allow-Methods"] "*")))))
 
